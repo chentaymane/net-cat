ad98e3db76aa498e86d5919857cff5896febf9f0
achent <159848379+chentaymane@users.noreply.github.com>
2026-01-15 17:13:00 +0100

    Implement message validation function
    Added a new function to validate messages based on allowed characters.

--- functions/sub-functions.go
+++ functions/sub-functions.go
@@ -2,6 +2,7 @@ package functions
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -13,17 +14,44 @@ func sendPrompt(c *Client) {
 
 func sendPromptLocked(c *Client) {
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	fmt.Fprintf(c.conn, "\x1b[1;37m[%s][%s]:\x1b[0m", timestamp, c.name)
+	fmt.Fprintf(c.conn, "[%s][%s]:", timestamp, c.name)
 }
 
 func validName(name string) bool {
 	mu.Lock()
 	defer mu.Unlock()
-
+	if name == "" || len(name) > 10 {
+		return false
+	}
+	for _, r := range name {
+		// Si le caractère n'est PAS une minuscule ET n'est PAS une majuscule ET n'est PAS un chiffre
+		if !(r >= 'a' && r <= 'z') && !(r >= 'A' && r <= 'Z') && !(r >= '0' && r <= '9') {
+			return false
+		}
+	}
 	for _, c := range clients {
 		if c.name == name {
 			return false
 		}
 	}
+	return true
+}
+
+func validMsg(msg string) bool {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if msg == "" || len(msg) > 100 {
+		return false
+	}
+
+	// Caractères autorisés : lettres, chiffres, espaces et ponctuation courante
+
+	for _, r := range msg {
+		if !strings.ContainsRune(allowedChars, r) {
+			return false
+		}
+	}
+
 	return true
 }
